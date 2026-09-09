2d655847a4f5325c4bbc8bb43b8420aac38a53dc
mdatif6197 <mdatif6197@gmail.com>
2026-09-09 17:04:36 +0530

    Time: 2 ms (99.65%) | Memory: 43.7 MB (99.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+
+        if( s.length() != t.length()){
+            return false;
+        }
         int[] freq = new int[26];
 
             for(char c: s.toCharArray()){
